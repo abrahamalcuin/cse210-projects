21802e3826f1e0cb982bd136a195df050e1996e8
abrahamal <abrahamalcuin@gmail.com>
2026-09-23 21:26:21 +0800

    Added prep 4 and prep 5 to turn in

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List <int> numbers = new List<int>(); 
+        int tracker = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (tracker == 0)
+        {
+            Console.WriteLine("Enter number: ");
+            int userNum = int.Parse(Console.ReadLine());
+            if (userNum == 0)
+            {
+                tracker = 1;
+            }
+            else
+            {
+            numbers.Add(userNum);
+            }
+        }
+        int total = numbers.Sum();
+        Console.WriteLine($"The Sum is: {total}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average is {average}");
+
+        int largest = numbers.Max();
+        Console.WriteLine($"The largest number is {largest}");
+
+
+
+
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,55 @@ using System;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your Name?");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite Number?");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+    static int PromptUserBirthYear()
+    {
+        Console.WriteLine("Please enter the year you were born:");
+        int userBirthYear = int.Parse(Console.ReadLine());
+        return userBirthYear;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int SquaredNumber = number*number;
+        return SquaredNumber;
+
+    }
+
+    static void DisplayResult()
+    {
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int userBirthYear = PromptUserBirthYear();
+        int SquaredNumber = SquareNumber(userNumber);
+        int YearsOld = DateTime.Now.Year - userBirthYear;
+
+        Console.WriteLine($"{userName}, the square of your number is {SquaredNumber}");
+        Console.WriteLine($"{userName}, you will turn / have turned {YearsOld} this year.");
+        
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult();
     }
 }
