0b69a10fe9fdbfcfc1214ec1c35933bb072c7f7b
abrahamal <abrahamalcuin@gmail.com>
2026-09-23 20:02:46 +0800

    Wrote prep 3 program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       int magicNum;
+       int userInt;
+       int tryCount = 0;
+
+    //    Console.WriteLine("What is the magic number?"); 
+    //    magicNum = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        magicNum = randomGenerator.Next(1,100);
+        
+       Console.WriteLine("What is your guess?"); 
+        userInt = int.Parse(Console.ReadLine());
+
+       while (userInt != magicNum)
+        {
+        
+            if (userInt > magicNum){
+                Console.WriteLine("Lower");
+                Console.WriteLine("What is your guess?");
+                userInt = int.Parse(Console.ReadLine());
+                
+            }
+            else if (userInt <magicNum){
+                Console.WriteLine("Higher");
+                Console.WriteLine("What is your guess?");
+                userInt = int.Parse(Console.ReadLine());
+                
+            }
+            tryCount++;
+        }
+        Console.WriteLine($"Congratulations! You guessed it with {tryCount} tries!");
     }
 }
